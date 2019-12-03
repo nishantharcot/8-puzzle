066160bcec84facbe3d03c2446526ce111beb08c
Arcot Nishanth <nsharc4@gmail.com>
2019-12-03 23:52:05 +0530

    opening page initialized

---
+++ assets/js/app.ts
@@ -0,0 +1,22 @@
+(
+  document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      const model = {
+
+      };
+      const view = {
+        init : () => {
+
+        },
+        render : () => {
+
+        }
+      };
+      const controller = {
+        init : () => {
+          view.init();
+        }
+      };
+    }
+  }
+)();
