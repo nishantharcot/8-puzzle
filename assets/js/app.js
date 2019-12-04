f0d01427f602fc0eb1bd1ec4dd18e2e3c34cdf9f
Arcot Nishanth <nsharc4@gmail.com>
2019-12-05 00:24:19 +0530

    empty slide movement corrected

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,34 @@
 (document.onreadystatechange = function () {
     if (document.readyState === 'complete') {
         var gameImages_1 = document.getElementsByClassName('game-images');
-        // console.log(gameImages[0].children);
-        var gameBox = document.getElementById('gamebox');
-        // console.log(gameBox.children[0].children);
+        var gameBox_1 = document.getElementById('gamebox');
         var gameSlides_1 = document.getElementsByClassName('slide');
-        var model = {};
+        var shuffleButton_1 = document.getElementsByClassName('shuffle');
+        var solveButton_1 = document.getElementsByClassName('solve');
+        var neighbours = {
+            // col + row
+            '00': [gameSlides_1[1], gameSlides_1[3]],
+            '01': [gameSlides_1[0], gameSlides_1[2], gameSlides_1[4]],
+            '02': [gameSlides_1[1], gameSlides_1[5]],
+            '10': [gameSlides_1[0], gameSlides_1[4], gameSlides_1[6]],
+            '11': [gameSlides_1[1], gameSlides_1[3], gameSlides_1[5], gameSlides_1[7]],
+            '12': [gameSlides_1[2], gameSlides_1[4], gameSlides_1[8]],
+            '20': [gameSlides_1[3], gameSlides_1[7]],
+            '21': [gameSlides_1[4], gameSlides_1[6], gameSlides_1[8]],
+            '22': [gameSlides_1[5], gameSlides_1[7]]
+        };
+        var model_1 = {
+            available: neighbours['22'],
+            neighbours: neighbours,
+            emptyRow: 2,
+            emptyColumn: 2,
+            currentImageindex: '0'
+        };
         var view_1 = {
             init: function () {
-                // console.log(gameImages[0].children[0]);
-                var Image1 = gameImages_1[0].children[0];
-                // console.log(Image1);
                 var allImages = gameImages_1[0].children;
                 var startImages = allImages[0].children;
                 var startImage = startImages[0];
-                console.log(startImage);
                 var sourceHeight = Math.floor(startImage.naturalHeight / 3);
                 var sourceWidth = Math.floor(startImage.naturalWidth / 3);
                 var slideWidth = Math.floor(gameSlides_1[0].clientWidth);
@@ -37,11 +51,20 @@
                         }
                     }
                 }
+                view_1.render();
+            },
+            render: function () {
+                var allImages = gameImages_1[0].children;
+                // console.log(model.available.length);
+                for (var i = 0; i < model_1.available.length; i++) {
+                    // console.log('Hello!!');
+                    model_1.available[i].onclick = controller_1.move;
+                }
                 for (var index = 0; index < allImages.length; index++) {
                     allImages[index].onclick = controller_1.loadImage;
                 }
-            },
-            render: function () {
+                shuffleButton_1[0].onclick = controller_1.shuffle;
+                solveButton_1[0].onclick = controller_1.shuffle;
             }
         };
         var controller_1 = {
@@ -50,12 +73,11 @@
             },
             loadImage: function (e) {
                 var gameImage = e.target;
-                // console.log(gameImage.clientHeight);
+                model_1.currentImageindex = gameImage.getAttribute('index');
                 var sourceHeight = Math.floor(gameImage.naturalHeight / 3);
                 var sourceWidth = Math.floor(gameImage.naturalWidth / 3);
                 var slideWidth = Math.floor(gameSlides_1[0].clientWidth);
                 var slideHeight = Math.floor(gameSlides_1[0].clientHeight);
-                // console.log(gameSlides);
                 for (var row = 0; row < 3; row += 1) {
                     for (var col = 0; col < 3; col += 1) {
                         if (row === 2 && col === 2) {
@@ -72,6 +94,40 @@
                             var context = canvas.getContext("2d");
                             context.drawImage(gameImage, col * sourceWidth, row * sourceHeight, sourceWidth, sourceHeight, 0, 0, slideWidth, slideHeight);
                         }
+                    }
+                }
+            },
+            shuffle: function () {
+            },
+            solve: function () {
+            },
+            move: function (e) {
+                var canvas = e.target;
+                var clickedSlide = canvas.parentElement;
+                for (var i = 0; i < model_1.available.length; i++) {
+                    // console.log(clickedSlide.parentNode, model.available[i]);
+                    if (clickedSlide === model_1.available[i]) {
+                        var row = Number(clickedSlide.getAttribute('row'));
+                        var col = Number(clickedSlide.getAttribute('col'));
+                        var empty = gameBox_1.children[model_1.emptyColumn].children[model_1.emptyRow];
+                        // console.log(empty);
+                        var slideCanvas = clickedSlide.children[0];
+                        var slideContext = slideCanvas.getContext('2d');
+                        var emptyCanvas = empty.children[0];
+                        var emptyContext = emptyCanvas.getContext('2d');
+                        var tempCanvas = document.createElement('canvas');
+                        var tempContext = tempCanvas.getContext('2d');
+                        tempCanvas.width = slideCanvas.width;
+                        tempCanvas.height = slideCanvas.height;
+                        tempContext.drawImage(slideCanvas, 0, 0);
+                        slideContext.drawImage(emptyCanvas, 0, 0);
+                        emptyContext.drawImage(tempCanvas, 0, 0);
+                        model_1.emptyColumn = col;
+                        model_1.emptyRow = row;
+                        var req = '';
+                        req = String(model_1.emptyColumn) + String(model_1.emptyRow);
+                        model_1.available = model_1.neighbours[req];
+                        view_1.render();
                     }
                 }
             }
--- assets/js/app.ts
+++ assets/js/app.ts
@@ -4,16 +4,32 @@
       const gameImages = document.getElementsByClassName('game-images') as HTMLCollectionOf<HTMLImageElement>;
       const gameBox = document.getElementById('gamebox') as HTMLDivElement;
       const gameSlides = document.getElementsByClassName('slide') as HTMLCollectionOf<HTMLDivElement>;
+      const shuffleButton = document.getElementsByClassName('shuffle') as HTMLCollectionOf<HTMLButtonElement>;
+      const solveButton = document.getElementsByClassName('solve') as HTMLCollectionOf<HTMLButtonElement>;
+      const neighbours = {
+        // col + row
+        '00' :  [gameSlides[1], gameSlides[3]],
+        '01' :  [gameSlides[0], gameSlides[2], gameSlides[4]],
+        '02' :  [gameSlides[1], gameSlides[5]],
+        '10' :  [gameSlides[0], gameSlides[4], gameSlides[6]],
+        '11' :  [gameSlides[1], gameSlides[3], gameSlides[5], gameSlides[7]],
+        '12' :  [gameSlides[2], gameSlides[4], gameSlides[8]],
+        '20' :  [gameSlides[3], gameSlides[7]],
+        '21' :  [gameSlides[4], gameSlides[6], gameSlides[8]],  
+        '22' :  [gameSlides[5], gameSlides[7]]
+      }
       const model = {
-
+        available : neighbours['22'] as HTMLDivElement[],
+        neighbours: neighbours,
+        emptyRow : 2,
+        emptyColumn : 2,
+        currentImageindex : '0',
       };
       const view = {
         init : () => {
-          const Image1 = gameImages[0].children[0] as HTMLAnchorElement;
           const allImages = gameImages[0].children as HTMLCollectionOf<HTMLImageElement>;
-          let startImages = allImages[0].children as HTMLCollectionOf<HTMLImageElement>;
+          const startImages = allImages[0].children as HTMLCollectionOf<HTMLImageElement>;
           const startImage = startImages[0] as HTMLImageElement;
-          console.log(startImage);
           const sourceHeight = Math.floor(startImage.naturalHeight / 3);
           const sourceWidth = Math.floor(startImage.naturalWidth / 3);
           const slideWidth = Math.floor(gameSlides[0].clientWidth);
@@ -37,12 +53,22 @@
               }
             }
           }
+          view.render();
+        },
+        render : () => {
+          const allImages = gameImages[0].children as HTMLCollectionOf<HTMLImageElement>;
+          // console.log(model.available.length);
+          for (let i = 0; i < model.available.length; i++) {
+            // console.log('Hello!!');
+            model.available[i].onclick = controller.move;
+          }
 
           for (let index = 0; index < allImages.length; index++) {
             allImages[index].onclick = controller.loadImage;
           }
-        },
-        render : () => {
+
+          shuffleButton[0].onclick = controller.shuffle;
+          solveButton[0].onclick = controller.shuffle;
 
         }
       };
@@ -52,6 +78,7 @@
         },
         loadImage: (e: MouseEvent) => {
           let gameImage = e.target as HTMLImageElement;
+          model.currentImageindex = gameImage.getAttribute('index');
           const sourceHeight = Math.floor(gameImage.naturalHeight / 3);
           const sourceWidth = Math.floor(gameImage.naturalWidth / 3);
           const slideWidth = Math.floor(gameSlides[0].clientWidth);
@@ -75,6 +102,47 @@
             }
           }
         },
+        shuffle : () => {
+        },
+        solve : () => {
+
+        },
+        move : (e: MouseEvent) => {
+
+          const canvas = e.target as HTMLCanvasElement;
+          const clickedSlide = canvas.parentElement as HTMLDivElement;
+          for (let i = 0; i < model.available.length; i++) {
+            // console.log(clickedSlide.parentNode, model.available[i]);
+            if (clickedSlide === model.available[i]) {
+              const row = Number(clickedSlide.getAttribute('row'));
+              const col = Number(clickedSlide.getAttribute('col'));
+              const empty = gameBox.children[model.emptyColumn].children[model.emptyRow] as HTMLDivElement;
+
+              const slideCanvas = clickedSlide.children[0] as HTMLCanvasElement;
+              const slideContext = slideCanvas.getContext('2d');
+
+              const emptyCanvas = empty.children[0] as HTMLCanvasElement;
+              const emptyContext = emptyCanvas.getContext('2d');
+
+              const tempCanvas = document.createElement('canvas');
+              const tempContext = tempCanvas.getContext('2d');
+
+              tempCanvas.width = slideCanvas.width;
+              tempCanvas.height = slideCanvas.height;
+              tempContext.drawImage(slideCanvas, 0, 0);
+              slideContext.drawImage(emptyCanvas, 0, 0);
+              emptyContext.drawImage(tempCanvas, 0, 0);
+              
+
+              model.emptyColumn = col;
+              model.emptyRow = row;
+              let req = '';
+              req = String(model.emptyColumn) + String(model.emptyRow);
+              model.available = model.neighbours[req];
+              view.render()
+            }
+          }
+        }
       }
       controller.init();
     }
